af106102c38547777e94c29f69d0ceaad6658e96
climatescience <130755391+climatescience@users.noreply.github.com>
2023-06-26 18:55:39 +0500

    Add files via upload
    This code will extract the values of corresponding pixels using the point shape file.

---
+++ ExtractPointValues.R
@@ -0,0 +1,28 @@
+#*******************************************#
+# NCDF to GeoTiff conversion
+#
+# created by: Dr. Usman Mazhar
+#             usmanmazhargeo@outlook.com
+#*******************************************#
+library(sp)
+library(raster)
+library(rgdal)
+
+mypath = "D:/test/"
+setwd(mypath)
+
+# stack all raster files from your working directory.
+mylist = list.files(mypath, pattern = "*.tif", full.names = T)
+mylist
+mystack = stack(mylist)
+
+# Read the point shape file. 
+mypoint = readOGR("random100.shp")
+
+# Extract the corresponding pixel values.
+myvalue = extract(mystack, mypoint)
+
+# Write the output as a csv.
+
+write.csv(myvalue, "point_value.csv")
+Sys.time()
